77065a3f953899cfc0ea0d92ddf11028e9af9bd0
curt <curt.k@kakaomobility.com>
2021-07-22 14:09:56 +0900

    minor

--- websocket/src/main/java/example/ClientMain.java
+++ websocket/src/main/java/example/ClientMain.java
@@ -2,6 +2,8 @@ package example;
 
 import java.net.URI;
 import java.time.Duration;
+
+import org.springframework.web.reactive.socket.WebSocketHandler;
 import org.springframework.web.reactive.socket.WebSocketMessage;
 import org.springframework.web.reactive.socket.client.ReactorNettyWebSocketClient;
 import org.springframework.web.reactive.socket.client.WebSocketClient;
@@ -14,17 +16,18 @@ public class ClientMain {
 
     WebSocketClient client = new ReactorNettyWebSocketClient();
 
-    client.execute(
-        URI.create("ws://localhost:8080/echo"),
-        session -> session.send(
-            Mono.just(session.textMessage("event-spring-reactive-client-websocket"))
-        ).thenMany(
-            session.receive().doOnNext(msg -> {
-              System.out.println("--> receive from server" + msg.getPayloadAsText());
-            })
-        )
-        .then()
-    )
+    WebSocketHandler handler = session -> {
+      return session.send(
+          Mono.just(session.textMessage("event-spring-reactive-client-websocket"))
+      ).thenMany(
+          session.receive().doOnNext(msg -> {
+            System.out.println("--> receive from server" + msg.getPayloadAsText());
+          })
+      )
+      .then();
+    };
+
+    client.execute(URI.create("ws://localhost:8080/echo"), handler)
     .block(Duration.ofSeconds(10L));
   }
 }
