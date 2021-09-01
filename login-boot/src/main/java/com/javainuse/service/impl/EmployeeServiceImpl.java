03431a12513f5107c78c3774cbcde00e0af5b8f8
curt.k <curt.k@kakaocorp.com>
2021-09-01 12:51:28 +0900

    login-boot추가

---
+++ login-boot/src/main/java/com/javainuse/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,38 @@
+package com.javainuse.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.javainuse.dao.EmployeeDao;
+import com.javainuse.model.Employee;
+import com.javainuse.service.EmployeeService;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+	@Autowired
+	EmployeeDao employeeDao;
+
+	@Override
+	public void insertEmployee(Employee employee) {
+		employeeDao.insertEmployee(employee);
+	}
+
+	@Override
+	public void insertEmployees(List<Employee> employees) {
+		employeeDao.insertEmployees(employees);
+	}
+
+	public List<Employee> getAllEmployees() {
+		return employeeDao.getAllEmployees();
+	}
+
+	@Override
+	public void getEmployeeById(String empId) {
+		Employee employee = employeeDao.getEmployeeById(empId);
+		System.out.println(employee);
+	}
+
+}
