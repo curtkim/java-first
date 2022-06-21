e71cf6423dff2b9788e58215b553f5ba12938e6b
curt <curt.k@kakaomobility.com>
2022-06-21 14:13:28 +0900

    nio add

--- nio/src/test/java/example/WriteInputStreamToFile.java
+++ nio/src/test/java/example/WriteInputStreamToFile.java
@@ -1,4 +1,25 @@
 package example;
 
+
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.nio.file.StandardCopyOption;
+
 public class WriteInputStreamToFile {
+
+  @Test
+  public void whenConvertingAnInProgressInputStreamToFile_thenCorrect2() throws IOException {
+
+    InputStream initialStream = new FileInputStream(new File("build.gradle"));
+    File targetFile = new File("build.tmp");
+
+    java.nio.file.Files.copy(
+        initialStream,
+        targetFile.toPath(),
+        StandardCopyOption.REPLACE_EXISTING);
+
+    IOUtils.closeQuietly(initialStream);
+  }
 }
