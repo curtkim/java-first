ccac71a7f338dcbab89863f182818829bace4c5d
curt <curt.k@kakaomobility.com>
2022-06-21 17:28:04 +0900

    minor

--- spring-data-reactive-redis/src/test/java/lettuce/MultiDao.java
+++ spring-data-reactive-redis/src/test/java/lettuce/MultiDao.java
@@ -18,30 +18,27 @@ public class MultiDao {
   private GenericObjectPool<StatefulRedisConnection<String, String>> pool;
 
   Mono<TransactionResult> doMulti(){
-    StatefulRedisConnection<String, String> conn = null;
     try {
-      conn = pool.borrowObject();
+      StatefulRedisConnection<String, String> conn = pool.borrowObject();
+      RedisReactiveCommands<String, String> cmd = conn.reactive();
+
+      logger.info("multi {}", conn.isOpen());
+      return cmd.multi()
+          .doOnSuccess(s -> {
+            logger.info("multi inner");       // multi와는 다른 thread에서 실행된다. lettuce가 생성한 thread에서 실행된다.
+            cmd.set("key", "1").subscribe();
+            cmd.incr("key").subscribe();
+          }).flatMap(s -> {
+            logger.info("exec");
+            return cmd.exec();
+          })
+          .log()
+          .doFinally(s -> {
+            logger.info("doFinally return Object");
+            pool.returnObject(conn);
+          });
     } catch (Exception e) {
       return Mono.error(e);
     }
-    final StatefulRedisConnection<String, String> finalConn = conn;
-
-    RedisReactiveCommands<String, String> cmd = conn.reactive();
-
-    logger.info("multi {}", conn.isOpen());
-    return cmd.multi()
-        .doOnSuccess(s -> {
-          logger.info("multi inner");       // multi와는 다른 thread에서 실행된다. lettuce가 생성한 thread에서 실행된다.
-          cmd.set("key", "1").subscribe();
-          cmd.incr("key").subscribe();
-        }).flatMap(s -> {
-          logger.info("exec");
-          return cmd.exec();
-        })
-        .log()
-        .doFinally(s -> {
-          logger.info("doFinally return Object");
-          pool.returnObject(finalConn);
-        });
   }
 }
