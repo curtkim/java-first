1ed78b771296b1d2d4abcb1f0b676d02ef44f64f
curt <curt.k@kakaomobility.com>
2022-06-27 12:21:04 +0900

    restTemplate에서 lettuce connection 꺼내기

--- data_redis_test_minimun/src/test/java/example/MyRedisTemplateTest.java
+++ data_redis_test_minimun/src/test/java/example/MyRedisTemplateTest.java
@@ -1,5 +1,6 @@
 package example;
 
+import io.lettuce.core.api.async.RedisAsyncCommands;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
@@ -7,7 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.annotation.Order;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SessionCallback;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -17,6 +23,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MyRedisTemplateTest {
   @Autowired
   private RedisTemplate redisTemplate;
+
+  @Test
+  public void test(){
+    redisTemplate.executeWithStickyConnection(new RedisCallback() {
+      @Override
+      public Object doInRedis(RedisConnection connection) throws DataAccessException {
+        RedisAsyncCommands commands = (RedisAsyncCommands)connection.getNativeConnection();
+        System.out.println(connection.getNativeConnection().getClass().getName());
+        return null;
+      }
+    });
+  }
 
   @Test
   public void t1_set(){
