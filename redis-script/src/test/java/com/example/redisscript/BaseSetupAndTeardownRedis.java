c36a2f871c817148f220bcc35eec83be2fe8a7f1
curt <curt.k@kakaomobility.com>
2022-07-08 14:24:42 +0900

    test containers test추가

---
+++ redis-script/src/test/java/com/example/redisscript/BaseSetupAndTeardownRedis.java
@@ -0,0 +1,32 @@
+package com.example.redisscript;
+
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.api.reactive.RedisReactiveCommands;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.utility.DockerImageName;
+
+@Testcontainers
+public class BaseSetupAndTeardownRedis {
+  @Container
+  public static GenericContainer genericContainer = new GenericContainer(
+      DockerImageName.parse("redis:5.0.3-alpine")
+  ).withExposedPorts(6379);
+
+  protected RedisClient redisClient;
+  protected RedisReactiveCommands<String, String> redisReactiveCommands;
+
+  @BeforeEach
+  public void setupRedisClient() {
+    redisClient = RedisClient.create("redis://" + genericContainer.getHost() + ":" + genericContainer.getMappedPort(6379));
+    redisReactiveCommands = redisClient.connect().reactive();
+  }
+
+  @AfterEach
+  public void removeAllDataFromRedis() {
+    redisClient.connect().reactive().flushall().block();
+  }
+}
