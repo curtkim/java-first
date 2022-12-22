fff7d84d2bd573fc4c9ac019227e133e53f4a9eb
curt <curt.k@kakaomobility.com>
2022-12-22 17:28:17 +0900

    minor

--- kryo-first/src/main/java/object/ObjectIO.java
+++ kryo-first/src/main/java/object/ObjectIO.java
@@ -7,15 +7,50 @@ import com.esotericsoftware.kryo.io.KryoObjectOutput;
 import com.esotericsoftware.kryo.io.Output;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ObjectIO {
 
   public static void main(String[] args) throws IOException, ClassNotFoundException {
     doWithFile();
+
+    doByList();
+  }
+
+  static void doByList() throws IOException, ClassNotFoundException {
+    String fileName = "books_list.data";
+
+    Kryo kryo = new Kryo();
+    kryo.register(Book.class);
+    kryo.register(Arrays.asList().getClass());
+
+    {
+      // write two record
+      FileOutputStream fos = new FileOutputStream(fileName);
+      Output output = new Output(fos);
+
+      KryoObjectOutput objectOutput = new KryoObjectOutput(kryo, output);
+      objectOutput.writeObject(Arrays.asList(
+              new Book(1, "one"),
+              new Book(2, "two"),
+              new Book(3, "three")
+      ));
+      objectOutput.close();
+
+      FileInputStream fis = new FileInputStream(fileName);
+      Input input = new Input(fis);
+      KryoObjectInput objectInput = new KryoObjectInput(kryo, input);
+      List<Book> books = (List<Book>)objectInput.readObject();
+      for(Book book : books)
+        System.out.println(book);
+    }
   }
 
   static void doWithFile() throws IOException, ClassNotFoundException {
