dc26b507c2161e2e04d453c9c0cb19e2f5177ee6
curt <curt.k@kakaomobility.com>
2023-04-26 16:36:18 +0900

    redis cluster상에서 transaction(multi ~ exec) 성공

--- redis-testcontainer-cluster/src/test/java/com/example/LettuceClusterTransactionTest.java
+++ redis-testcontainer-cluster/src/test/java/com/example/LettuceClusterTransactionTest.java
@@ -2,10 +2,15 @@ package com.example;
 
 import com.redis.testcontainers.RedisClusterContainer;
 import io.lettuce.core.RedisURI;
+import io.lettuce.core.TransactionResult;
+import io.lettuce.core.api.sync.RedisCommands;
 import io.lettuce.core.cluster.ClusterClientOptions;
 import io.lettuce.core.cluster.ClusterTopologyRefreshOptions;
 import io.lettuce.core.cluster.RedisClusterClient;
+import io.lettuce.core.cluster.SlotHash;
 import io.lettuce.core.cluster.api.StatefulRedisClusterConnection;
+import io.lettuce.core.cluster.api.sync.RedisAdvancedClusterCommands;
+import io.lettuce.core.cluster.models.partitions.RedisClusterNode;
 import io.lettuce.core.support.ConnectionPoolSupport;
 import org.apache.commons.pool2.impl.GenericObjectPool;
 import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
@@ -14,6 +19,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -36,7 +42,7 @@ public class LettuceClusterTransactionTest {
 
     ClusterTopologyRefreshOptions topologyRefreshOptions = ClusterTopologyRefreshOptions.builder()
         .enableAdaptiveRefreshTrigger(ClusterTopologyRefreshOptions.RefreshTrigger.MOVED_REDIRECT, ClusterTopologyRefreshOptions.RefreshTrigger.PERSISTENT_RECONNECTS)
-        //.adaptiveRefreshTriggersTimeout(30, TimeUnit.SECONDS)
+        .adaptiveRefreshTriggersTimeout(Duration.ofSeconds(30))
         .build();
 
     clusterClient.setOptions(ClusterClientOptions.builder()
@@ -48,8 +54,23 @@ public class LettuceClusterTransactionTest {
         new GenericObjectPoolConfig()
     );
 
+    // https://groups.google.com/g/lettuce-redis-client-users/c/llH4sfkUj8k
+    String key = "{user101}/drive";
+    String key2 = "{user101}/path";
+
     try (StatefulRedisClusterConnection<String, String> connection = pool.borrowObject()) {
-      connection.sync().set("key", "value");
+      RedisClusterNode node = connection.getPartitions().getPartitionBySlot(SlotHash.getSlot(key));
+      System.out.println(node);
+      RedisCommands<String, String> commands = connection.getConnection(node.getNodeId()).sync();
+      {
+        commands.multi();
+        commands.set(key, "1");
+        commands.set(key2, "2");
+        TransactionResult result = commands.exec();
+
+        System.out.println(result);
+        result.stream().forEach(it -> System.out.println(it));
+      }
     }
 
     pool.close();
