8c16a1e31619d838df41d74a2524af9b62eaa785
curt <curt.k@kakaomobility.com>
2023-06-08 13:41:59 +0900

    minor

--- memory-mapped-first/src/main/java/org/example/big_file_read/ByMemoryMapped.java
+++ memory-mapped-first/src/main/java/org/example/big_file_read/ByMemoryMapped.java
@@ -6,23 +6,25 @@ import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 
 public class ByMemoryMapped {
-  private static final String FILE_NAME = "links_capital_mesh_conn3.txt";
+  private static final String FILE_NAME = "links_mesh_conn3.txt";
 
   public static void main(String[] args) throws IOException {
     long startTime = System.currentTimeMillis();
-    RandomAccessFile file = new RandomAccessFile(FILE_NAME, "rw");
+    RandomAccessFile file = new RandomAccessFile(FILE_NAME, "r");
+
     FileChannel channel = file.getChannel();
+    System.out.println("Reading content and printing ... " + channel.size() + " bytes");
 
     // Read file into mapped buffer
     MappedByteBuffer mbb =
         channel.map(FileChannel.MapMode.READ_ONLY,
             0,          // position
             channel.size());
+    System.out.println("capacity " + mbb.capacity());
 
-    System.out.println("Reading content and printing ... " + channel.size() + " bytes");
-    for (int i = 0; i < channel.size(); i++) {
-      //System.out.print((char) mbb.get());
-      mbb.get();
+    byte[] bytes = new byte[2];
+    while(mbb.hasRemaining()) {
+      mbb.get(bytes, 0, Math.min(mbb.capacity() - mbb.position(), bytes.length));
     }
     System.out.println(String.format("total=%d free=%d", Runtime.getRuntime().totalMemory(), Runtime.getRuntime().freeMemory()));
 
