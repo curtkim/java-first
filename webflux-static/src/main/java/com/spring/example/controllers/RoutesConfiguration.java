751a9d11a26089e250273e8e485d42aa0718a727
curt <curt.k@kakaomobility.com>
2023-06-26 09:14:32 +0900

    webflux-static 최신화, 결국 RouterFunction을 추가해서 해결

--- webflux-static/src/main/java/com/spring/example/controllers/RoutesConfiguration.java
+++ webflux-static/src/main/java/com/spring/example/controllers/RoutesConfiguration.java
@@ -4,9 +4,12 @@ import com.spring.example.models.Person;
 import com.spring.example.repositories.PersonRespository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.method;
 import static org.springframework.web.reactive.function.server.RequestPredicates.path;
@@ -16,19 +19,23 @@ import static org.springframework.web.reactive.function.server.ServerResponse.ok
 
 @Configuration
 public class RoutesConfiguration {
+  @Bean
+  RouterFunction<ServerResponse> staticResourceRouter() {
+    return RouterFunctions.resources("/**", new FileSystemResource("public/"));
+  }
 
-    @Bean
-    RouterFunction<?> routes(PersonRespository personRespository) {
-        return nest(path("/person"),
+  @Bean
+  RouterFunction<?> routes(PersonRespository personRespository) {
+    return nest(path("/person"),
 
-          route(RequestPredicates.GET("/{id}"),
+        route(RequestPredicates.GET("/{id}"),
             request -> ok().body(personRespository.findById(request.pathVariable("id")), Person.class))
 
-          .andRoute(method(HttpMethod.POST),
-              request -> {
-                personRespository.insert(request.bodyToMono(Person.class)).subscribe();
-                return ok().build();
-              })
-        );
-    }
+            .andRoute(method(HttpMethod.POST),
+                request -> {
+                  personRespository.insert(request.bodyToMono(Person.class)).subscribe();
+                  return ok().build();
+                })
+    );
+  }
 }
