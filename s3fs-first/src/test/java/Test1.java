d407e3bdc18be76f93cbfdce2e2de922fdd7385c
curt <curt.k@kakaomobility.com>
2023-07-09 11:16:04 +0900

    minor

---
+++ s3fs-first/src/test/java/Test1.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Test1 {
+
+  @Test
+  public void test(){
+    assertEquals(1,1);
+  }
+}
