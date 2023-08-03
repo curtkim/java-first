31c99cd7c452e53b47ef11642d3715aea1ff00b9
curt <curt.k@kakaomobility.com>
2023-08-03 11:10:49 +0900

    protobuf-spring List<Protobuf>는 에러를 발생시킨다.

--- protobuf-spring/src/main/java/com/example/protobufspring/CourseController.java
+++ protobuf-spring/src/main/java/com/example/protobufspring/CourseController.java
@@ -2,9 +2,13 @@ package com.example.protobufspring;
 
 import com.baeldung.protobuf.BaeldungTraining;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class CourseController {
@@ -14,5 +18,10 @@ public class CourseController {
   @RequestMapping("/courses/{id}")
   BaeldungTraining.Course customer(@PathVariable Integer id) {
     return courseRepo.getCourse(id);
+  }
+
+  @GetMapping("/courses/")
+  List<BaeldungTraining.Course> customerAll(){
+    return new ArrayList<>(courseRepo.courses.values());
   }
 }
