555c2d74840627175f66d1c323861db647387c5b
curt <curt.k@kakaomobility.com>
2023-08-14 01:06:50 +0900

    minor

--- sux4j-first/src/main/java/org/example/sux/Ex03_LcpMonotoneMinimalPerfectHashFunction.java
+++ sux4j-first/src/main/java/org/example/sux/Ex03_LcpMonotoneMinimalPerfectHashFunction.java
@@ -1,20 +1,19 @@
 package org.example.sux;
 
 import it.unimi.dsi.bits.TransformationStrategies;
-import it.unimi.dsi.sux4j.mph.GOVMinimalPerfectHashFunction;
 import it.unimi.dsi.sux4j.mph.LcpMonotoneMinimalPerfectHashFunction;
 
 import java.io.IOException;
-import java.util.Arrays;
+import java.io.Serializable;
 import java.util.List;
-import java.util.stream.Collectors;
 
-// origin_value -> idx(순차적이지는 않음)
+// origin_value -> idx(순차적임)
 public class Ex03_LcpMonotoneMinimalPerfectHashFunction {
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException, ClassNotFoundException {
 
     List<Long> list = Utils.makeRandomSortedList(3_000_000, 10_000_000_000L);
+
     {
       LcpMonotoneMinimalPerfectHashFunction.Builder<Long> builder = new LcpMonotoneMinimalPerfectHashFunction.Builder<>();
       LcpMonotoneMinimalPerfectHashFunction<Long> fun = builder
@@ -23,13 +22,19 @@ public class Ex03_LcpMonotoneMinimalPerfectHashFunction {
           .build();
 
       System.out.println("numBits=" + fun.numBits());
-      System.out.println("numBytes=" + fun.numBits()/8);
+      System.out.println("numBytes=" + fun.numBits() / 8);
       System.out.println("size64=" + fun.size64());
       for (Long a : list.subList(0, 10))
         System.out.println(a + " " + fun.getLong(a));
 
+      Utils.writeFile("lcp.obj", fun);
+      Utils.writeFile("lcp_origin.obj", (Serializable)list);
+    }
 
-      //Utils.writeFile("lcp.obj", fun);
+    {
+      LcpMonotoneMinimalPerfectHashFunction<Long> fun = (LcpMonotoneMinimalPerfectHashFunction<Long>) Utils.readFile("lcp.obj");
+      for (Long a : list.subList(0, 10))
+        System.out.println(a + " " + fun.getLong(a));
     }
   }
 }
